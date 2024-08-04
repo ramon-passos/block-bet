dbed5ff5bc44b023f8f06a6929e9ed301861b7fe
ramon-passos <ramonpassos2011@gmail.com>
2024-08-04 16:07:18 -0300

    wip: creating bet

--- server/contracts/BlockBet.sol
+++ server/contracts/BlockBet.sol
@@ -3,46 +3,95 @@
 pragma solidity ^0.8.13;
 
 contract BlockBet {
+    enum WinnerVote {
+        UNDEFINED,
+        OWNER,
+        CHALLENGER
+    }
+
+    enum Status {
+        OPEN,
+        CHALLENGED,
+        FINESHED,
+        CANCELED
+    }
+
     struct Punter {
-        address punter_address;
+        address punterAddress;
         bool decision;
-        bool winner_vote;
+        WinnerVote winnerVote;
     }
 
     struct OracleDecision {
-        address oracle_address;
-        bool oracle_decision;
+        address orableAddress;
+        bool oracleDecision;
     }
 
     struct Bet {
+        string uuid;
         uint value;
         string description;
         address result;
         bool invalid;
         Punter owner;
         Punter challenger;
-        Punter[] punter_votes;
+        Punter[] punterVotes;
         OracleDecision[] oracles;
+        Status status;
     }
 
-	Bet bets;
+	Bet[] bets;
     mapping (address => uint) reputations;
+    address public escrow;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 	constructor() {
-		bets[tx.origin] = 10000;
+		escrow = address(this);
 	}
 
-    function createBet(uint value, string description) public returns(bool sufficient) {
-        if (bets[msg.sender] < value) return false;
-        bets[msg.sender] -= value;
-        bets[receiver] += value;
-        emit Transfer(msg.sender, receiver, value);
+    function createBet(uint value, bool ownerDecision, string memory description) public returns(bool sufficient) {
+        require(value > 0, "Value must be greater than 0");
+
+        Punter memory owner = Punter({
+            punterAddress: msg.sender,
+            decision: ownerDecision,
+            winnerVote: WinnerVote.UNDEFINED
+        });
+
+        Punter memory emptyChallenger = Punter({
+            punterAddress: address(0),
+            decision: false,
+            winnerVote: WinnerVote.UNDEFINED
+        });
+
+        Bet memory newBet = Bet({
+            uuid: generateUUID(),
+            value: value,
+            description: description,
+            result: address(0),
+            invalid: false,
+            owner: owner,
+            challenger: new Challenger,
+            punterVotes: new Punter[](0),
+            oracles: new OracleDecision[](0)
+        });
+
+        bets.push(newBet);
+        emit Transfer(msg.sender, escrow, value);
+        
         return true;
     }
 
-    function getBet() public returns(Bet bet) {
+    function getBet(string memory uuid) public returns(Bet memory bet) {
+        for (uint i = 0; i < bets.length; i++) {
+            if (keccak256(abi.encodePacked(bets[i].uuid)) == keccak256(abi.encodePacked(uuid))) {
+                return bets[i];
+            }
+        }
+    }
+
+    /* function getBets(address searcher) public returns(Bet[] bets) {
 
     }
 
@@ -56,6 +105,10 @@ contract BlockBet {
 
     function auditBet() {
 
+    } */
+
+    function generateUUID() private returns(string memory) {
+        
     }
 }
 
